02f7b6be025c43673c99bb276b17f46aae0bcf0b
Mark Harwood <mark@phunky.co.uk>
2016-04-22 09:44:59 +0100

    Gah bloody github oauth token fail

---
+++ link/.config/fish/conf.d/path.fish
@@ -0,0 +1,2 @@
+# Paths
+set -x PATH                      /usr/local/bin $PATH
--- link/.config/fish/config.fish
+++ link/.config/fish/config.fish
@@ -2,17 +2,3 @@
 for file in ~/.config/fish/conf.d/*.fish
     source $file
 end
-
-# Paths
-set -x PATH                      /usr/local/bin $PATH
-
-# Add my dotfiles to my path, could symlink these but don't want  to have to rerun setup just for this.
-set -x PATH                      $PATH ~/dotfiles/bin
-
-# Larger bash history (allow 32³ entries; default is 500)
-set -x HISTSIZE                  32768
-set -x HISTFILESIZE              $HISTSIZE
-set -x HISTCONTROL               ignoredups
-
-# Github OAuth Token for Homebrew
-set -x HOMEBREW_GITHUB_API_TOKEN da779371bfd9b0ebea33d2c1e5355503b6885193
